7258919abf84a31102902f07d409b835ab3b215a
Jeriko <jcarrico1985@live.com>
2017-05-31 13:24:06 -0500

    Created and added more values to albums_seeder.sql

---
+++ albums_seeder.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES
+  ('Meat Loaf', 'Bat Out of Hell', '1977', 43, 'Hard rock, Progressive rock'),
+  ('Eagles', 'Their Greatest Hits (1971–1975)', '1976', 42, 'Rock, Soft rock, Folk rock'),
+  ('Bee Gees', 'Saturday Night Fever', '1977', 40, 'Disco'),
+  ('Fleetwood Mac', 'Rumours', '1977', 40, 'Soft Rock'),
+  ('Shania Twain', 'Come On Over', '1997', 39, 'Country, Pop');
+
+
