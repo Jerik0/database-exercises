6a97aa3f54e7331847c93070a77229ce00f12c93
Jeriko <jcarrico1985@live.com>
2017-05-31 15:03:17 -0500

    Added selection for 'rock' genre

--- select_exercise.sql
+++ select_exercise.sql
@@ -6,5 +6,8 @@ SELECT * FROM albums WHERE artist = 'Pink Floyd';
 SELECT 'The genre for ''Back In Black''' AS 'Caption';
 SELECT genre FROM albums WHERE name = 'Back In Black';
 
-SELECT 'All albums released in the 90''s';
+SELECT 'All albums released in the 90''s' AS 'Caption';
 SELECT * FROM albums WHERE release_date BETWEEN 1990 and 1999;
+
+SELECT 'All albums in the ''rock'' genre' AS 'Caption';
+SELECT name FROM albums WHERE genre LIKE '%rock%';
