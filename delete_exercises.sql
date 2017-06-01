75df8cbdd749a868b8ea0046f2a6b81334ed7fed
Jeriko <jcarrico1985@live.com>
2017-06-01 10:16:33 -0500

    Changed SELECT commands to DELETE commands.

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,11 +1,11 @@
 USE codeup_test_db;
 
-SELECT 'All albums released after 1991' AS '';
-SELECT name FROM albums WHERE release_date > '1991';
+SELECT 'Delete All albums released after 1991' AS '';
+DELETE FROM albums WHERE release_date > '1991';
 
-SELECT 'All albums in the pop genre' AS '';
-SELECT name FROM albums WHERE genre LIKE '%pop%';
+SELECT 'Delete All albums in the pop genre' AS '';
+DELETE FROM albums WHERE genre LIKE '%pop%';
 
-SELECT 'All albums released by Celine Dion' AS '';
-SELECT name FROM albums WHERE artist LIKE '%celine%';
+SELECT 'Delete All albums released by Celine Dion' AS '';
+DELETE FROM albums WHERE artist LIKE '%celine%';
 
