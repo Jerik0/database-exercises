4df6e63bab95bdd9b5c2a6da1c79217dddc4d5f4
Jeriko <jcarrico1985@live.com>
2017-06-01 16:31:36 -0500

    updated query: last name contains 'e' will now give only unique results and will only give last name as result (as opposed to first and last name).

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -17,11 +17,11 @@ WHERE last_name LIKE 'E%'
       OR last_name LIKE '%e';
 
 SELECT 'All employees who''s last name begins AND ends with ''E''' AS '';
-SELECT first_name, last_name
+SELECT DISTINCT last_name
 FROM employees
 WHERE last_name LIKE 'E%'
       AND last_name LIKE '%e'
-ORDER BY emp_no DESC;
+GROUP BY last_name;
 
 SELECT 'All employees who were hired in the 90''s AND born on Christmas' AS '';
 SELECT *
