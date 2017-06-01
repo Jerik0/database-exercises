a0862289868fe61ddf6e0b5d9d220cfdd21e5a49
Jeriko <jcarrico1985@live.com>
2017-06-01 15:23:36 -0500

    Updated queries to be more specific.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,17 +1,31 @@
-SELECT 'All employees who''s first name is Irena, Vidya, or Maya' AS '';
-SELECT first_name, last_name
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+USE employees;
 
-SELECT 'All employees who''s last name begins with ''E''' AS '';
+SELECT 'All *male* employees who''s first name is Irena, Vidya, or Maya' AS '';
+SELECT first_name, last_name, gender
+FROM employees
+WHERE (
+  first_name = 'Irena'
+  OR first_name = 'Vidya'
+  OR first_name = 'Maya')
+AND gender = 'M';
+
+SELECT 'All employees who''s last name begins OR ends with ''E''' AS '';
 SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%e';
+
+SELECT 'All employees who''s last name begins AND ends with ''E''' AS '';
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%e';
 
 SELECT 'All employees hired in the 90''s' AS '';
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31';
+WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31'
+AND birth_date LIKE '%-12-25';
 
 SELECT 'All employees who were born on Christmas' AS '';
 SELECT *
@@ -21,4 +35,5 @@ WHERE birth_date LIKE '%-12-25';
 SELECT 'All employees who''s last name contains the letter ''q''' AS '';
 SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+AND NOT last_name LIKE '%qu%';
